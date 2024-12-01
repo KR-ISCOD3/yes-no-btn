2b95a3bb0d206cdf28cc22c8326aefbcc24f4aff
KR-ISCOD3 <krisjenxz@gmail.com>
2024-12-01 13:16:57 +0700

    first commit code

---
+++ script.js
@@ -0,0 +1,16 @@
+let btnNo = document.getElementById("btnNo")
+
+btnNo.onmouseover = ()=>{
+
+    let innerWidth = window.innerWidth
+    let innerHeight = window.innerHeight
+
+    // console.log(innerWidth);
+    // console.log(innerHeight);
+
+    let randomX = Math.floor( Math.random() * (innerWidth - 100));
+    let randomY = Math.floor( Math.random() * (innerHeight - 50));
+    
+    btnNo.style.left = `${randomX}px`
+    btnNo.style.top = `${randomY}px`
+}
